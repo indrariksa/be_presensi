b83fef32fce937faabe9bab7e66713c5a29ab719
Indra Riksa Herlambang <indrariksa@gmail.com>
2023-08-22 19:37:10 +0700

    add ubah dan delete

--- module/contact.go
+++ module/contact.go
@@ -38,3 +38,40 @@ func InsertKontak(db *mongo.Database, col string, nmkontak string, nmrkontak str
 	insertedID = result.InsertedID.(primitive.ObjectID)
 	return insertedID, nil
 }
+
+func UpdateKontak(db *mongo.Database, col string, id primitive.ObjectID, nmkontak string, nmrkontak string, almt string, ktrngn string) (err error) {
+	filter := bson.M{"_id": id}
+	update := bson.M{
+		"$set": bson.M{
+			"namakontak": nmkontak,
+			"nomorhp":    nmrkontak,
+			"alamat":     almt,
+			"keterangan": ktrngn,
+		},
+	}
+	result, err := db.Collection(col).UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		fmt.Printf("Update Kontak: %v\n", err)
+		return
+	}
+	if result.ModifiedCount == 0 {
+		return fmt.Errorf("data with ID %s not found", id)
+	}
+	return nil
+}
+
+func DeleteKontakByID(_id primitive.ObjectID, db *mongo.Database, col string) error {
+	kontak := db.Collection(col)
+	filter := bson.M{"_id": _id}
+
+	result, err := kontak.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return fmt.Errorf("error deleting data for ID %s: %s", _id, err.Error())
+	}
+
+	if result.DeletedCount == 0 {
+		return fmt.Errorf("data with ID %s not found", _id)
+	}
+
+	return nil
+}
